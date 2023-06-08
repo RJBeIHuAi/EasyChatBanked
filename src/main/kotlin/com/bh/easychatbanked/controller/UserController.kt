a58aee8b624b30a5d02db67f58606adb725fc1dd
RJBeIHuAi <2841953564@qq.com>
2023-06-08 23:27:17 +0800

    修改对用户操作的返回  ApiResponse

--- src/main/kotlin/com/bh/easychatbanked/controller/UserController.kt
+++ src/main/kotlin/com/bh/easychatbanked/controller/UserController.kt
@@ -1,6 +1,5 @@
 package com.bh.easychatbanked.controller
 
-import com.bh.easychatbanked.eneity.User
 import com.bh.easychatbanked.request.RegistrationRequest
 import com.bh.easychatbanked.request.UserUpdateRequest
 import com.bh.easychatbanked.service.UserService
@@ -18,7 +17,7 @@ class UserController(
     private val userService: UserService,
 ){
     @PostMapping("/register")
-    fun registerUser(@RequestBody registrationRequest: RegistrationRequest): ResponseEntity<String> {
+    fun registerUser(@RequestBody registrationRequest: RegistrationRequest): ResponseEntity<ApiResponse> {
         // 从请求体中获取用户注册信息
         val account = registrationRequest.account
         val username = registrationRequest.username
@@ -30,35 +29,50 @@ class UserController(
         val existingUser = userService.getUserByAccount(account)
         if (existingUser != null) {
             // 账号已经存在，返回相应的错误信息
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Account already exists.")
+            val response = ApiResponse(HttpStatus.BAD_REQUEST.value(), "Account already exists.")
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
         }
 
         // 调用 UserService 注册用户
         userService.registerUser(account, username, password, email, phone)
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully.")
+        val response = ApiResponse(HttpStatus.CREATED.value(), "User registered successfully.")
+        return ResponseEntity.status(HttpStatus.CREATED).body(response)
     }
 
     @GetMapping("/find/{id}")
-    fun getUserById(@PathVariable id: Long?): User? {
-        return userService.getUserById(id)
+    fun getUserById(@PathVariable id: Long?): ResponseEntity<ApiResponse> {
+        val user = userService.getUserById(id)
+        return if (user != null) {
+            val response = ApiResponse(HttpStatus.OK.value(), "User found.")
+            ResponseEntity.ok(response)
+        } else {
+            val response = ApiResponse(HttpStatus.NOT_FOUND.value(), "User not found.")
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
+        }
     }
 
     @DeleteMapping("/del/{id}")
-    fun deleteUser(@PathVariable id: Long): ResponseEntity<String> {
-        userService.deleteUserById(id)
-        return ResponseEntity.status(HttpStatus.CREATED).body("User deleted successfully.")
+    fun deleteUser(@PathVariable id: Long): ResponseEntity<ApiResponse> {
+        return try {
+            userService.deleteUserById(id)
+            ResponseEntity.ok(ApiResponse(200, "User deleted successfully."))
+        } catch (e: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse(500, "Failed to delete user."))
+        }
     }
     @PatchMapping("/update/{id}")
     fun updateUser(
         @PathVariable id: Long,
         @RequestBody updatedUser: UserUpdateRequest
-    ): ResponseEntity<String> {
+    ): ResponseEntity<ApiResponse> {
         val success = userService.updateUser(id, updatedUser)
         return if (success) {
-            ResponseEntity.ok("User updated successfully.")
+            val response = ApiResponse(HttpStatus.OK.value(), "User updated successfully.")
+            ResponseEntity.ok(response)
         } else {
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.")
+            val response = ApiResponse(HttpStatus.NOT_FOUND.value(), "User not found.")
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
         }
     }
 
@@ -68,3 +82,5 @@ class UserController(
     }
 
 }
+
+data class ApiResponse(val code: Int, val message: String)
